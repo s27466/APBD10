3603138e96c9c119e67cff21c770b5ef313ba958
s24666 <s27466@pjwstk.edu.pl>
2024-06-19 20:30:38 +0200

    scrap, od nowa

--- WebApplication3/WebApplication3/DTOs/PatientDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication3.DTOs;
-
-public class PatientDTO
-{
-    public int IdPatient { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public DateTime Birthdate { get; set; }
-}
